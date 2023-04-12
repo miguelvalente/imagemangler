1fac533ae112208173f3fa2ea7f5675232554a91
miguelvalente <miguelvalente@protonmail.com>
2023-04-12 13:57:42 +0200

    refactor: :art: Deleted __main__ and simplified deteriorate

--- imagemangler/core/mangler.py
+++ imagemangler/core/mangler.py
@@ -3,33 +3,17 @@ import io
 
 
 def deteriorate(
-    image_path: str, iterations: int = 1, optimize: bool = False, quality: int = 20
-) -> None:
-    """Deteriorate the image by 50% quality for `iterations` number of times"""
+    img: Image, optimize: bool = True, quality: int = 20
+) -> Image:
+    """Deteriorate the image quality for `iterations` number of times"""
 
-    # Open the original image file
-    with open(image_path, "rb") as f:
-        # Load the image from the file
-        img = Image.open(io.BytesIO(f.read()))
+    compressed_buffer = io.BytesIO()
+    img.save(compressed_buffer, format="JPEG", optimize=optimize, quality=quality)
 
-    for i in range(iterations):
-        # Compress the image
-        compressed_buffer = io.BytesIO()
-        img.save(compressed_buffer, format="JPEG", optimize=optimize, quality=quality)
+    # Get the compressed image data as bytes
+    compressed_bytes = compressed_buffer.getvalue()
 
-        # Get the compressed image data as bytes
-        compressed_bytes = compressed_buffer.getvalue()
+    # Load the compressed image into a PIL.Image object
+    img = Image.open(io.BytesIO(compressed_bytes))
 
-        # Load the compressed image into a PIL.Image object
-        img = Image.open(io.BytesIO(compressed_bytes))
-
-    # Load the original image into a PIL.Image object
-    original_img = Image.open(io.BytesIO(open(image_path, "rb").read()))
-
-    # Show the original and final deteriorated images
-    original_img.show()
-    img.show()
-
-
-if __name__ == "__main__":
-    deteriorate("imagemangler/book.webp", 5)
+    return img
